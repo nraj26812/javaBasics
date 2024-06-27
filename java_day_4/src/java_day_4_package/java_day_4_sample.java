ff43e748cf139fdb72a99059c321440cdce73b6a
nraj2 <nraj2@NAVEEN_RAJ>
2024-06-27 11:43:12 +0530

    added some

--- java_day_4/src/java_day_4_package/java_day_4_sample.java
+++ java_day_4/src/java_day_4_package/java_day_4_sample.java
@@ -4,7 +4,23 @@ public class java_day_4_sample{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hello world");
+System.out.println("Hello world");
+//Assigned single character literal
+      char ch='a';
+      System.out.println(ch);
+ //Assigned number to char
+      char ch1=65;
+      System.out.println(ch1);
+    //Assigned unicode to char
+      char ch2='\u00A7';
+      System.out.println(ch2);
+      //Assigned representation
+      char ch3='\u20AC';
+      System.out.println(ch3);
+      //ASIC code representation
+      int a='A';
+      System.out.println(a);
+      
 	}
 
 }
