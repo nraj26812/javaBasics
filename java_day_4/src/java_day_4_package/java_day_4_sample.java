f298d734f6ad33301e9ca34e4f73b27af5fd9392
nraj2 <nraj2@NAVEEN_RAJ>
2024-06-27 11:07:15 +0530

    This is my basic java program

---
+++ java_day_4/src/java_day_4_package/java_day_4_sample.java
@@ -0,0 +1,10 @@
+package java_day_4_package;
+
+public class java_day_4_sample{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Hello world");
+	}
+
+}
