2b3d4361f2a0bb831657639143303eaf01c0f95a
nraj2 <nraj2@NAVEEN_RAJ>
2024-07-15 09:50:42 +0530

    added some

---
+++ java_day_4/src/day_7_overriding/ICICI.java
@@ -0,0 +1,11 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package day_7_overriding;
+
+//subclass
+public class ICICI extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.9f;
+	}
+
+}
---
+++ java_day_4/src/day_7_overriding/RBI.java
@@ -0,0 +1,10 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package day_7_overriding;
+
+//Superclass
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
